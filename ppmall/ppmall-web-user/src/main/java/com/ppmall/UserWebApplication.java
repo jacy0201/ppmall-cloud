acf1f8df6e76aad9e841028a9c5a332772f477aa
rex_lee <lxailcquwer@qq.com>
2018-07-19 17:08:35 +0800

    /

--- ppmall/ppmall-web-user/src/main/java/com/ppmall/UserWebApplication.java
+++ ppmall/ppmall-web-user/src/main/java/com/ppmall/UserWebApplication.java
@@ -1,11 +1,15 @@
 package com.ppmall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableRedisHttpSession
+@MapperScan("com.ppmall.dao")
 public class UserWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserWebApplication.class, args);
